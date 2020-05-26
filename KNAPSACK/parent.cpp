d55e63b4102f9b1cbf754093c40e7f8207967fc9
Saurav Gupta <saurav@saurav-lappy>
2020-05-26 19:51:16 +0530

    complete_without_memoization

--- KNAPSACK/parent.cpp
+++ KNAPSACK/parent.cpp
@@ -7,7 +7,9 @@ int knapsack(int wt[], int val[], int W, int n)
   if (n == 0 || W == 0)
     return 0;
 
-  // Implementing choice diamgram
+  // Implementing choice diamgram 
+  // /DP/KNAPSACK/parent_kanpsack_choice.png
+  
   if (wt[n - 1] <= W)
     return max(val[n - 1] + knapsack(wt, val, W - wt[n - 1], n - 1),
                knapsack(wt, val, W, n - 1));
