bd9905324af3b3203bf8c4e92be7fb45d0f4e7aa
Saurav Gupta <saurav@saurav-lappy>
2020-05-26 19:40:20 +0530

    knapsack code implemented

--- KNAPSACK/parent.cpp
+++ KNAPSACK/parent.cpp
@@ -7,14 +7,20 @@ int knapsack(int wt[], int val[], int W, int n)
   if (n == 0 || W == 0)
     return 0;
 
-  
+  // Implementing choice diamgram
+  if (wt[n - 1] <= W)
+    return max(val[n - 1] + knapsack(wt, val, W - wt[n - 1], n - 1),
+               knapsack(wt, val, W, n - 1));
+
+  else
+    return knapsack(wt, val, W, n - 1);
 }
 
 int main()
 {
-  int wt[] = {};
-  int val[] = {};
-  int W = 7;
-  int n = 4;
+  int val[] = {60, 100, 120};
+  int wt[] = {10, 20, 30};
+  int W = 50;
+  int n = 3;
   cout << knapsack(wt, val, W, n);
 }
